8eaa71a566ed667f7b6a73f48aa70ae3d4a4b742
IslenaCaldas <islenacaldas@gmail.com>
2024-09-12 11:14:33 -0500

    filtros

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+const yearSelect= document.getElementById('yearFilter');
+const countrySelect= document.getElementById('countryFilter');
 const container = document.getElementById("grid-container");
 container.appendChild(renderItems(data));
 
